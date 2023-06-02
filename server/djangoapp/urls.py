e498db9f5ff2b89e4ed6c52e3a9b389a55d314b0
sp <staticpogona@gmail.com>
2023-06-02 13:50:39 -0400

    temporary changes

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,10 +8,14 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='base/', view=views.base_view, name='base'),
 
     # path for about view
+    path('about/', views.about, name='about'),
 
     # path for contact us view
+    path('contact/', views.contact, name='contact'),
+
 
     # path for registration
 
