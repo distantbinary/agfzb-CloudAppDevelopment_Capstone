4b277d108f6c9af8a41e005bb4127c70ae6fa356
sp <staticpogona@gmail.com>
2023-06-03 06:53:37 -0400

    in progress

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -2,29 +2,26 @@
  * Get all databases
  */
 
- const { CloudantV1 } = require("@ibm-cloud/cloudant");
- const { IamAuthenticator } = require("ibm-cloud-sdk-core");
+const { CloudantV1 } = require("@ibm-cloud/cloudant");
+const { IamAuthenticator } = require("ibm-cloud-sdk-core");
  
- function main(params) {
-   const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
-   const cloudant = CloudantV1.newInstance({
-     authenticator: authenticator,
-   });
-   cloudant.setServiceUrl(params.COUCH_URL);
+function main(params) {
+    const cloudant = Cloudant({
+        url: params.COUCH_URL,
+        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+    });
+    let dbListPromise = getDbs(cloudant);
+    return dbListPromise;
+}
  
-   let dbList = getDbs(cloudant);
-   return { dbs: dbList };
- }
- 
- function getDbs(cloudant) {
-   cloudant
-     .getAllDbs()
-     .then((body) => {
-       body.forEach((db) => {
-         dbList.push(db);
-       });
-     })
-     .catch((err) => {
-       console.log(err);
-     });
- }
+function getDbs(cloudant) {
+    return new Promise((resolve, reject) => {
+        cloudant.db.list()
+            .then(body => {
+                resolve({ dbs: body });
+            })
+            .catch(err => {
+                reject({ err: err });
+            });
+    });
+}
