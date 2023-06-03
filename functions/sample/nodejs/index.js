08e6edaf7a0304b1febce926fb8e2b40a147a7a3
sp <staticpogona@gmail.com>
2023-06-03 13:39:51 -0400

    partial done

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -4,24 +4,27 @@
 
 const { CloudantV1 } = require("@ibm-cloud/cloudant");
 const { IamAuthenticator } = require("ibm-cloud-sdk-core");
- 
+
 function main(params) {
-    const cloudant = Cloudant({
-        url: params.COUCH_URL,
-        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
-    });
-    let dbListPromise = getDbs(cloudant);
-    return dbListPromise;
+  const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
+  const cloudant = CloudantV1.newInstance({
+    authenticator: authenticator,
+  });
+  cloudant.setServiceUrl(params.COUCH_URL);
+
+  let dbList = getDbs(cloudant);
+  return { dbs: dbList };
 }
- 
+
 function getDbs(cloudant) {
-    return new Promise((resolve, reject) => {
-        cloudant.db.list()
-            .then(body => {
-                resolve({ dbs: body });
-            })
-            .catch(err => {
-                reject({ err: err });
-            });
+  cloudant
+    .getAllDbs()
+    .then((body) => {
+      body.forEach((db) => {
+        dbList.push(db);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
 }
--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
-# Register your models here.
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+
+admin.site.register(CarMake, CarMakeAdmin)
+
+
 
 # CarModelInline class
-
 # CarModelAdmin class
-
 # CarMakeAdmin class with CarModelInline
-
 # Register models here
